a7ee57f7a1ffa794cb675dc7330dccc128d48ed3
mrphupham88 <phupv@nevagroup.vn>
2020-03-06 23:00:22 +0700

    first commit

---
+++ ServiceDemo/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceDemo.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet, Route("")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("This is get product API");
+        }
+    }
+}
---
+++ ServiceExample/Controllers/PaymentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceExample.Controllers
+{
+    [Route("api/payment")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        [HttpGet,Route("")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("This is get payment API");
+        }
+    }
+}
